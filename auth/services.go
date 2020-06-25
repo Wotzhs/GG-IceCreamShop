0fa48f3d3e814f64c6c21a0ed40285cfb826b106
Sean Wong <wotzhs@gmail.com>
2020-06-25 19:14:21 +0800

    auth: implement generate jwt token service

--- auth/services.go
+++ auth/services.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-	"context"
-	"proto/auth"
+	"log"
+	"os"
+	"time"
+
+	"github.com/SermoDigital/jose/crypto"
+	"github.com/SermoDigital/jose/jws"
 )
+
+var (
+	authService  *AuthService
+	jwtSecretKey string
+)
+
+func init() {
+	if jwtSecretKey = os.Getenv("JWT_SECRET_KEY"); jwtSecretKey == "" {
+		log.Fatalf("env JWT_SECRET_KEY not set")
+	}
+}
 
 type AuthService struct{}
 
-func (s *AuthService) GenerateJWTToken(ctx context.Context, req *auth.Credentials) (*auth.JWTToken, error) {
-	return &auth.JWTToken{
-		JwtToken: "hello word",
-	}, nil
+func (s *AuthService) GenerateToken(email string) (string, error) {
+	claims := jws.Claims{}
+	claims.Set("email", email)
+	claims.SetExpiration(time.Now().Add(24 * time.Hour))
+	claims.SetIssuedAt(time.Now())
+
+	jwt := jws.NewJWT(claims, crypto.SigningMethodHS256)
+
+	token, err := jwt.Serialize([]byte(jwtSecretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return string(token), nil
 }

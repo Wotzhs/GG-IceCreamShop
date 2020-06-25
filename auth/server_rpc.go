1868c9e01ca8aee9c0984d805875e19f468b1298
Sean Wong <wotzhs@gmail.com>
2020-06-25 19:14:49 +0800

    auth: rename previously named auth service to auth server rpc

---
+++ auth/server_rpc.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"proto/auth"
+
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+type AuthServerRPC struct{}
+
+func (s *AuthServerRPC) GenerateJWTToken(ctx context.Context, req *auth.Credentials) (*auth.JWTToken, error) {
+	fmt.Printf("ctx: %+v\n", ctx)
+	md, _ := metadata.FromIncomingContext(ctx)
+	fmt.Printf("metadata %v\n", md)
+	resp, err := UserClient.GetPasswordHash(ctx, &auth.Credentials{Email: req.Email})
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "%v", err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(resp.Hash), []byte(req.Password)); err != nil {
+		return nil, status.Errorf(codes.NotFound, "%v", err)
+	}
+
+	token, err := authService.GenerateToken(req.Email)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "%v", err)
+	}
+
+	return &auth.JWTToken{JwtToken: token}, nil
+}

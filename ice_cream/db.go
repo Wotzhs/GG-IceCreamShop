a75e410e1ff8f8318d9e00bc318c9febbdb7075d
Sean Wong <wotzhs@gmail.com>
2020-06-26 12:56:31 +0800

    ice_cream: add db connection for ice_cream service

---
+++ ice_cream/db.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func init() {
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		dbURL = "postgresql://postgres@localhost:5433/postgres?sslmode=disable"
+	}
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("user db connection err: %v", err)
+	}
+
+	if err := conn.Ping(); err != nil {
+		log.Fatalf("failed to ping user db err: %v", err)
+	}
+
+	db = conn
+}
